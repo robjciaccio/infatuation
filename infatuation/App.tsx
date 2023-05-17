4161f9f9e5d194f4d05ec61fc4bc65e0fdd11e08
Robert Ciaccio <robjciaccio@gmail.com>
2023-05-17 15:44:43 -0600

    project started

---
+++ infatuation/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello Infatuation</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
